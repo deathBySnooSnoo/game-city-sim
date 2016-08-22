a78321cfeb3c9fb393812f67dbb2400c7ccb8e75
Davis <davis.bittner@gmail.com>
2016-08-21 22:14:30 -0500

    added a clock and calendar to gametime class

--- gametime.cpp
+++ gametime.cpp
@@ -1,3 +1,8 @@
+/**
+    Class for keeping time
+    Super long-term: different types of calendars for different users
+*/
+
 #include <sstream>
 #include <string>
 
@@ -12,13 +17,62 @@ class gameTime{
         int year;
         int hour;
         int minute;
+        bool paused;
+
+        //fuck leap years
+        void tick(){
+            while(!paused){
+                minute++;
+                if(minute == 60){
+                    hour++;
+                    minute = 0;
+                    if(hour == 24){
+                        hour = 0;
+                        day++;
+                        if(((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) || ((month == 2) && day == 29) || ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) && day == 32)){
+                            month++;
+                            day = 1;
+                        }
+                        else if(month == 12 && day == 32){
+                            month = 1;
+                            day = 1;
+                            year++;
+                        }
+                    }
+                }
+            }
+        }
 
     public:
         
+        //returns date as a string for convenience for now
         string getDate(){
             stringstream s;
             s<<month<<"-"<<day<<"-"<<year;
             return s.str();
+        }
+
+        //start new game clock; maybe should be a constructor
+        void startClock(){
+            month = 1;
+            day = 1;
+            year = 1;
+            hour = 7;
+            minute = 0;
+            
+        }
+
+        //start clock from save game; also maybe should be a constructor
+        void startClock(string time){
+            
+        }
+
+        void pause(){
+            paused = true;
+        }
+
+        void unpause(){
+            paused = false;
         }
 
 };
